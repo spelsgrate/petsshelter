67fd5e56c9769d0daa42934f62e36dfc3771dbd0
spelsgrate <antemratin@gmail.com>
2020-03-25 13:09:25 -0400

    added build folder

---
+++ client/build/precache-manifest.831ce9612c550240dbc368b7aac40b58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "314854dd008c59605286091b7809e5d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "263b13255df1f886dc63",
+    "url": "/static/css/main.c9be84da.chunk.css"
+  },
+  {
+    "revision": "24d62fcf9714be0cb6b3",
+    "url": "/static/js/2.c122797a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c122797a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "263b13255df1f886dc63",
+    "url": "/static/js/main.8cfeb3bf.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
